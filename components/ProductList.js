3227eba80e5108d8a8157d549394817b2e2d8a8d
Hritick Jaiswal <newbiehritick@gmail.com>
2021-10-03 17:41:12 +0530

    Added ProductList

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,9 +1,12 @@
 import Product from "./Product";
+import styles from "./ProductList.module.scss";
 
 function ProductList({ products }) {
   return (
-    <main>
+    <main className={styles.productList}>
       <Product product={products[0]} />
+      <Product product={products[1]} />
+      <Product product={products[2]} />
     </main>
   );
 }
