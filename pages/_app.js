fa71d6f307f118f34fa10ae3a8c7994f7688c7c4
Hritick Jaiswal <newbiehritick@gmail.com>
2021-10-03 06:10:54 +0530

    Added Head,title and description to every page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import { Fragment } from "react";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <title>Amazon Clone using Next js</title>
+        <meta name="description" content="Amazon clone using Next js" />
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
