2e6504b371feca6cf8b24ef6e96ca14e083f6628
Hritick Jaiswal <newbiehritick@gmail.com>
2021-10-03 17:49:26 +0530

    Finished Home Page --Frontend

--- components/ProductList.js
+++ components/ProductList.js
@@ -4,9 +4,9 @@ import styles from "./ProductList.module.scss";
 function ProductList({ products }) {
   return (
     <main className={styles.productList}>
-      <Product product={products[0]} />
-      <Product product={products[1]} />
-      <Product product={products[2]} />
+      {products.map((product) => (
+        <Product product={product} />
+      ))}
     </main>
   );
 }
